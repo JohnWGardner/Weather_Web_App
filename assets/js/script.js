050136c9b71c606d54e979f305a122585cb1de65
John Gardner <john.gardner@msn.com>
2024-10-19 15:53:07 +0000

    dev enviro setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("check").innerText = "JavaScript is working!";
+    document.getElementById("check").innerText = "JavaScript link is working";
 });
