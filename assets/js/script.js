b7959fd46b3a775cc47ceb7297e921b901e45ff3
Hodo Ismail <ic17oq5i@students.codeinstitute.net>
2024-10-22 13:06:40 +0000

    Create check weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("check").innerText = "JavaScript link is working";
-});
+const apiKey = "2b7ee3f098e996eea38e81986f8a163b";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBtn = document.getElementById("weatherBtn")
+const searchBox = document.getElementById("city");
+
+async function checkWeather(city) {
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    const data =  await response.json();
+
+    console.log(data)
+    document.querySelector(".card-title").innerHTML = data.name;
+    document.querySelector("#temperature").innerHTML = data.main.temp;
+    document.getElementById("condition").innerText = data.weather[0].main;
+  document.getElementById("humidity").innerText = `${data.current.humidity}%`;
+   
+}
+
+
+
+searchBtn.addEventListener("click", (e)=>{
+    // e.preventdefault();
+    checkWeather(searchBox.value);
+   
+})
